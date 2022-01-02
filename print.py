be327ae36c8cb6cc8632f1353c22dfe03c8e2914
Tien Phan <tien.phanthe@gmail.com>
2022-01-02 15:41:30 +0700

    Create print.py
    using print()

---
+++ print.py
@@ -0,0 +1,16 @@
+# using print() to print something 
+
+# print these messages 
+print("hello world!")
+
+# this print doesn't work, because doulbe quotes doesn't present this way. 
+#print("hello hello "hello hello"")
+
+# double quotes present by using double quotes in single quote. 
+print('hello "hello hello"')
+
+# also, I can use "\"message\""
+print("hello, this is \"a new quote\"")
+
+# or double escapes 
+print("The escape character is \\.")
