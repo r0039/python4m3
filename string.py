f6cc00e495a63c990bd8feae46874cd12581a307
Tien Phan <tien.phanthe@gmail.com>
2022-01-02 15:45:57 +0700

    Create string.py

---
+++ string.py
@@ -0,0 +1,11 @@
+"""
+In Python a string is either surrounded by double quotes ("Hello world") or single quotes ('Hello world'). 
+It doesn’t matter which kind you use, just be consistent.
+""" 
+
+# Examples of valid strings
+print("I'm coding!")
+print('The computer printed the string "Hello!"')
+
+# Example of invalid string 
+print("A common phrase in programming is "Hello world"")
