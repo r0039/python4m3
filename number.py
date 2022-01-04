e45207dd3fdff4e39a24c8af564f20da42c4e3a3
Tien Phan <tien.phanthe@gmail.com>
2022-01-04 07:49:24 +0700

    Create number.py

---
+++ number.py
@@ -0,0 +1,22 @@
+# An integer or `int` is a whole number. 
+# It has no decimal point and contains all counting numbers `(1, 2, 3, …)` as well as their negative counterparts and the number 0. 
+# If you were counting the number of people in a room, the number of jellybeans in a jar, or the number of keys on a keyboard you would likely use an integer.
+
+# A floating-point number, or a `float` is a decimal number. 
+# It can be used to represent fractional quantities as well as precise measurements. 
+# If you were measuring the length of your bedroom wall, calculating the average test score of a seventh-grade class, or storing a baseball player’s batting average for the 1998 season you would likely use a float.
+
+# Numbers can be assigned to variables or used literally in a program:    
+
+```
+an_int = 2 
+a_float = 2.1
+
+print(an_int + 3)
+# Output: 5
+```   
+
+# an integer
+# a float
+# 3 is a literal, meaning it’s actually the number 3 and not a variable with the number 3 assigned to it.
+
