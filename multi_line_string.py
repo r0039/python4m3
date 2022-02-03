aed3f34c2dd97a8d94815f488dba41f900080a8f
Tien Phan <tien.phanthe@gmail.com>
2022-02-03 16:49:39 +0700

    Create multi_line_string.py

---
+++ multi_line_string.py
@@ -0,0 +1,11 @@
+# Python offers a solution: multi-line strings. By using three quote-marks (""" or ''') instead of one 
+
+# Assign the string here
+to_you = """
+Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you?
+"""
+
+
+print(to_you)
