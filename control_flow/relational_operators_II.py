9087f190539bf1143dc3622ccbff831ff8c09690
Tien PHAN <tien.phanthe@gmail.com>
2022-02-06 20:53:15 +0700

    Relational Operators

---
+++ control_flow/relational_operators_II.py
@@ -0,0 +1,23 @@
+"""
+So far we know two relational operators, equals and not equals, but there are a ton (well, four) more:
+
+> greater than
+>= greater than or equal to
+< less than
+<= less than or equal to
+"""
+
+x = 20
+y = 20
+
+# Write the first if statement here:
+if x == y:
+  print("These numbers are the same")
+
+
+credits = 120
+
+# Write the second if statement here:
+if credits >= 120:
+  print("You have enough credits to graduate!")
+
