eeba0683bdcdf0e67d59e052e4814d92320e714d
Tien Phan <tien.phanthe@gmail.com>
2022-02-21 08:57:29 +0700

    Create What_can_a_List_contain.py

---
+++ INTRODUCTION_TO_LISTS/What_can_a_List_contain.py
@@ -0,0 +1,25 @@
+"""
+Lists can contain more than just numbers.
+
+Let’s revisit our classroom example with heights:
+
+Noelle is 61 inches tall
+Ava is 70 inches tall
+Sam is 67 inches tall
+Mia is 64 inches tall
+
+Instead of storing each student’s height, we can make a list that contains their names:
+names = ["Noelle", "Ava", "Sam", "Mia"]
+
+We can even combine multiple data types in one list. For example, this list contains both a string and an integer:
+mixed_list_string_number = ["Noelle", 61]
+
+Lists can contain any data type in Python! For example, this list contains a string, integer, boolean, and float.
+mixed_list_common = ["Mia", 27, False, 0.5]
+"""
+
+ints_and_strings = [1, 2, 3, "four", "five", "hallo"]
+print(ints_and_strings)
+
+sam_height_and_testscore = ["Sam", 67, 85.5, True]
+print(sam_height_and_testscore)
