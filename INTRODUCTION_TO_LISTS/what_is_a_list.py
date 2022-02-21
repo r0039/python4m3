18a2c32babca0ac9081efdc773a60fba3eea7512
Tien Phan <tien.phanthe@gmail.com>
2022-02-21 08:49:09 +0700

    Update what_is_a_list.py

---
+++ INTRODUCTION_TO_LISTS/what_is_a_list.py
@@ -0,0 +1,24 @@
+"""
+In programming, it is common to want to work with collections of data. 
+In Python, a list is one of the many built-in data structures that allows us to work with a collection of data in sequential order.
+
+Suppose we want to make a list of the heights of students in a class:
+
+* Noelle is 61 inches tall
+* Ava is 70 inches tall
+* Sam is 67 inches tall
+* Mia is 64 inches tall
+In Python, we can create a variable called heights to store these integers into a list:
+
+heights = [61, 70, 67, 64]
+
+Notice that:
+
+1/ A list begins and ends with square brackets ([ and ]).
+2/ Each item (i.e., 67 or 70) is separated by a comma (,)
+3/ It’s considered good practice to insert a space () after each comma, but your code will run just fine if you forget the space.
+"""
+
+heights = [61, 70, 67, 64, 65]
+
+broken_heights = [65, 71, 59, 62] 
