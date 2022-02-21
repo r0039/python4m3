310b50d069cbda4d80555cea181fb30cfc3681b7
Tien Phan <tien.phanthe@gmail.com>
2022-02-21 09:00:47 +0700

    Create empty_lists.py

---
+++ INTRODUCTION_TO_LISTS/empty_lists.py
@@ -0,0 +1,15 @@
+"""
+A list doesn’t have to contain anything. You can create an empty list like this:
+
+empty_list = []
+
+Why would we create an empty list?
+
+Usually, it’s because we’re planning on filling it up later based on some other input. We’ll talk about two ways of filling up a list in the next exercise.
+"""
+
+my_empty_list = []
+
+her_empty_list = []
+
+his_empty_list = []
