5371cf121aa2aa597111ad2119cf7c2ba8d86b16
Tien Phan <tien.phanthe@gmail.com>
2022-02-26 15:54:50 +0700

    Create Shrinking_a_List_Remove.py

---
+++ INTRODUCTION_TO_LISTS/Shrinking_a_List_Remove.py
@@ -0,0 +1,18 @@
+"""
+We can remove elements in a list using the .remove() Python method.
+"""
+
+# Your code below: 
+order_list = ["Celery", "Orange Juice", "Orange", "Flatbread"]
+print(order_list)
+order_list.remove("Flatbread")
+print(order_list)
+
+new_store_order_list = ["Orange", "Apple", "Mango", "Broccoli", "Mango"]
+print(new_store_order_list)
+
+new_store_order_list.remove("Mango")
+print(new_store_order_list)
+
+new_store_order_list.remove("Onions")
+print(new_store_order_list)
