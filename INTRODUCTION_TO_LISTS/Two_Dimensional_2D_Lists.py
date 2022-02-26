d7cae96be7d59f08ac29b0cfabbf9dcc24d9e641
Tien Phan <tien.phanthe@gmail.com>
2022-02-26 15:59:36 +0700

    Create Two_Dimensional_2D_Lists.py

---
+++ INTRODUCTION_TO_LISTS/Two_Dimensional_2D_Lists.py
@@ -0,0 +1,12 @@
+"""
+We’ve seen that the items in a list can be numbers or strings. Lists can contain other lists! We will commonly refer to these as two-dimensional (2D) lists.
+"""
+
+heights = [["Jenny", 61], ["Alexus", 70], ["Sam", 67], ["Grace", 64], ["Vik", 68]]
+print(heights)
+
+ages = [
+  ["Aaron", 15],
+  ["Dhruti", 16]
+]
+print(ages)
