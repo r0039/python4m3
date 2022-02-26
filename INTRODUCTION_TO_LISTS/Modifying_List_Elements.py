5a187036b96e1e23aa0dbabe66bb85d3bcfab9fa
Tien Phan <tien.phanthe@gmail.com>
2022-02-26 15:41:58 +0700

    Create Modifying_List_Elements.py

---
+++ INTRODUCTION_TO_LISTS/Modifying_List_Elements.py
@@ -0,0 +1,20 @@
+# Your code below:
+garden_waitlist = ["Jiho", "Adam", "Sonny", "Alisha"]
+print(garden_waitlist)
+
+garden_waitlist[1] = "Calla"
+print(garden_waitlist)
+
+garden_waitlist[-1] = "Alex"
+print(garden_waitlist)
+
+juventus = ["Nguyen Phu Trong", "Nguyen Xuan Phuc", "Tolam"]
+print(juventus)
+
+# Using narrative way
+juventus[0] = "Pham Minh Chinh"
+print(juventus)
+
+# Using negative way
+juventus[-1] = "Vuong Dinh Hue"
+print(juventus)
