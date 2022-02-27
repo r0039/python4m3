3590c05064ef56816eff31a7e3b9db20e6e02ac6
Tien Phan <tien.phanthe@gmail.com>
2022-02-27 12:10:47 +0700

    Create remove_in_2d_list.py

---
+++ INTRODUCTION_TO_LISTS/remove_in_2d_list.py
@@ -0,0 +1,7 @@
+"""
+To use the .remove() method on a two-dimensional list, call it on the sublist you are modifying and pass the value you want to remove in between the parenthesis ( ).
+"""
+practice_list = [["a"], ["b"], ["c"]]
+practice_list[1].remove("b")
+ 
+print(practice_list)
