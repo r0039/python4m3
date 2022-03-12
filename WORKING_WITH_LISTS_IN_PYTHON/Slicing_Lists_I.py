e4957cb127ca4be186cf860b8c6fc9606756c363
Tien Phan <tien.phanthe@gmail.com>
2022-03-12 16:14:52 +0700

    Create Slicing_Lists_I.py

---
+++ WORKING_WITH_LISTS_IN_PYTHON/Slicing_Lists_I.py
@@ -0,0 +1,14 @@
+"""
+In Python, often we want to extract only a portion of a list. Dividing a list in such a manner is referred to as slicing.
+"""
+
+suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+
+beginning = suitcase[0:4]
+print(beginning)
+
+# Your code below: 
+beginning = suitcase[0:2]
+
+middle = suitcase[2:4]
+print(middle)
