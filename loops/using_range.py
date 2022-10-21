4cd4dd8fdb3efc152ca817cbb6905c98b5fef7aa
Tien Phan <tien.phanthe@gmail.com>
2022-10-21 09:05:37 +0700

    Create using_range.py
    https://www.codecademy.com/paths/computer-science/tracks/cspath-cs-101/modules/cspath-python-loops/lessons/learn-python-loops/exercises/range

---
+++ loops/using_range.py
@@ -0,0 +1,11 @@
+promise = "I will finish the python loops module!"
+
+for temp in range(5):
+  print(promise + str(temp + 1))
+  
+
+##
+promise = "I will finish the python loops module!"
+
+for temp in range(5):
+  print(promise)
