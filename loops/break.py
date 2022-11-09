52ceb89a813f70e004a22271b1f2c356ff56ecac
Tien Phan <tien.phanthe@gmail.com>
2022-11-09 17:00:05 +0700

    Create break.py

---
+++ loops/break.py
@@ -0,0 +1,11 @@
+dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
+dog_breed_I_want = "dalmatian"
+
+print("Checking the sale list!")
+for dog_breed in dog_breeds_available_for_adoption:
+  print(dog_breed)
+  if dog_breed == dog_breed_I_want:
+    break
+
+print("They have the dog I want!")
+
