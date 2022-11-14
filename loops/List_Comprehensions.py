c686b826ae05ff55f796614e42875a33ffdbbe3b
Tien Phan <tien.phanthe@gmail.com>
2022-11-14 11:20:06 +0700

    Create List_Comprehensions.py
    https://www.codecademy.com/paths/computer-science/tracks/cspath-cs-101/modules/cspath-python-loops/lessons/learn-python-loops/exercises/list-comprehension-ii

---
+++ loops/List_Comprehensions.py
@@ -0,0 +1,32 @@
+# Example 1:
+grades = [90, 88, 62, 76, 74, 89, 48, 57]
+doubled = []
+scaled_grades = [num + 10 for num in grades]
+print(scaled_grades)
+
+for grade in grades:
+  doubled.append(grade * 2)
+print(doubled)
+
+
+# Example 2:
+grades = [90, 88, 62, 76, 74, 89, 48, 57]
+scaled_grades = [num + 10 for num in grades]
+print(scaled_grades)
+
+
+# Example 3: 
+numbers = [2, -1, 79, 33, -45]
+doubled = [num * 2 for num in numbers]
+print(doubled)
+
+
+# Example 4:
+numbers = [2, -1, 79, 33, -45]
+doubled = []
+ 
+for number in numbers:
+  doubled.append(number * 2)
+ 
+print(doubled)
+
